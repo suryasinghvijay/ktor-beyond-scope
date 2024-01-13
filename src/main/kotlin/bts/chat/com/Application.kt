a256c7d5499aabe5bbdad9228fb37e4deb1089db
Vijay <vijay>
2024-01-13 14:52:04 +0530

    added base project structure

---
+++ src/main/kotlin/bts/chat/com/Application.kt
@@ -0,0 +1,19 @@
+package bts.chat.com
+
+import bts.chat.com.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSockets()
+    configureSerialization()
+    configureRouting()
+    /asdasd
+}
